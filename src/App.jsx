26f76fc6150c00601643e85d366bb634dcf53a89
bryanteoh964 <68241796+bryanteoh964@users.noreply.github.com>
2023-08-26 00:46:55 -0400

    Fixing file set-up locations

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useRoutes } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>App</h1>
+    </>
+  )
+}
+
+export default App
