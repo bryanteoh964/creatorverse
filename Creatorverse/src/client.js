5acfd326a52b6f5b8cf10b0c31ba3a6cae7055ce
bryanteoh964 <68241796+bryanteoh964@users.noreply.github.com>
2023-08-26 00:31:20 -0400

    Added client for supabase database access

---
+++ Creatorverse/src/client.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const URL = "https://fjyssprovolkrjukfypc.supabase.co"
+const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZqeXNzcHJvdm9sa3JqdWtmeXBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMwMjM2ODYsImV4cCI6MjAwODU5OTY4Nn0.zyrQbWPIeVjCqy3B8v7UaYx-GoPGDPrm0lUV9a8_kFs"
+
+export const supabase = craeteClient(URL, API_KEY);
