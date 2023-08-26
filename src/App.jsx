1fb47aefd6bb50b25f0a62eac79cdbc55782f9c0
bryanteoh964 <68241796+bryanteoh964@users.noreply.github.com>
2023-08-26 01:31:28 -0400

    Set up routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
 import React from 'react'
 import { useRoutes } from 'react-router-dom'
+
+import Home from './pages/Home.jsx'
+import AddCreator from './pages/AddCreator.jsx'
+import EditCreator from './pages/EditCreator.jsx'
+import ShowCreators from './pages/ShowCreators.jsx'
+import ViewCreator from './pages/ViewCreator.jsx'
+
 import './App.css'
 
 function App() {
-
-  return (
-    <>
-      <h1>App</h1>
-    </>
-  )
+  let element = useRoutes([
+    {
+      path: "/",
+      element: <Home />,
+      children: [
+        {
+          path: "/add_creators",
+          element: <AddCreator />
+        },
+        {
+          path: "/edit_creators",
+          element: <EditCreator />
+        },
+        {
+          path: "/show_creators",
+          element: <ShowCreators />
+        }, 
+        {
+          path: "/view_creators",
+          element: <ViewCreator />
+        }
+      ]
+    }
+  ])
+  return element
 }
 
 export default App
